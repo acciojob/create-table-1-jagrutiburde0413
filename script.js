c94c903860f20b1fe5b460509e47105af55cd8ae
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-05-18 18:33:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function insert_Row() {
-    //Write your code here
+  // Create new elements
+  var newRow = document.createElement('tr');
+  var newCell1 = document.createElement('td');
+  var newCell2 = document.createElement('td');
   
+  // Set cell values
+  newCell1.textContent = 'New Cell1';
+  newCell2.textContent = 'New Cell2';
   
+  // Append cells to the new row
+  newRow.appendChild(newCell1);
+  newRow.appendChild(newCell2);
+  
+  // Get reference to the table
+  var table = document.getElementById('sampleTable');
+  
+  // Insert new row at the top
+  table.insertRow(0).appendChild(newRow);
 }
