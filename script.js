a19c294f3d5865080da4d2b21994efe424f916cb
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-05-18 18:44:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
+// function insert_Row() {
+//   // Create new elements
+//   var newRow = document.createElement('tr');
+//   var newCell1 = document.createElement('td');
+//   var newCell2 = document.createElement('td');
+  
+//   // Set cell values
+//   newCell1.textContent = 'New Cell1';
+//   newCell2.textContent = 'New Cell2';
+  
+//   // Append cells to the new row
+//   newRow.appendChild(newCell1);
+//   newRow.appendChild(newCell2);
+  
+//   // Get reference to the table body
+//   var table = document.getElementById('sampleTable');
+ 
+//   // Append new row to the table body
+//   table.insertRow(0).appendChild(newRow);
+// }
+
+
 function insert_Row() {
-  // Create new elements
-  var newRow = document.createElement('tr');
-  var newCell1 = document.createElement('td');
-  var newCell2 = document.createElement('td');
+  // Get reference to the table
+  var table = document.getElementById('sampleTable');
+  
+  // Insert new row at the first position
+  var newRow = table.insertRow(0);
+  
+  // Create new cells
+  var newCell1 = newRow.insertCell(0);
+  var newCell2 = newRow.insertCell(1);
   
   // Set cell values
   newCell1.textContent = 'New Cell1';
   newCell2.textContent = 'New Cell2';
-  
-  // Append cells to the new row
-  newRow.appendChild(newCell1);
-  newRow.appendChild(newCell2);
-  
-  // Get reference to the table body
-  var table = document.getElementById('sampleTable');
- 
-  // Append new row to the table body
-  table.insertRow(0).appendChild(newRow);
 }
